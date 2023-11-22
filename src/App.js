e7ce35697687257e179c0a4b11e7675844e29afa
Afsana Mimi <afsanamimi.wd@gmail.com>
2023-11-21 23:26:48 -0800

    Added Day picker

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Routes, Route, Link} from "react-router-dom";
 import Home from './pages/homepage/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
+import Footer from './pages/homepage/Footer';
+import Appointment from './pages/Appointment/Appointment';
 
 
 function App() {
@@ -13,8 +15,11 @@ function App() {
      <Routes>
       <Route path='/' element={<Home></Home>}></Route>
       <Route path='about' element={<About></About>}></Route>
+      <Route path='appointment' element={<Appointment></Appointment>}></Route>
       <Route path='login' element={<Login></Login>}></Route>
      </Routes>
+
+     
     </div>
   );
 }
